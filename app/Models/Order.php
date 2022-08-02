103efc6b46b4e22fcadbcbf6d3c9b24dee8edf74
dku1 <denis.kulagin1593@mail.ru>
2022-08-03 00:01:47 +0700

    order relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Order extends Model
 {
     use HasFactory;
+
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function isProcessed(): bool
+    {
+        return $this->status === 1;
+    }
 }
